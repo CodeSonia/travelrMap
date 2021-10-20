0b576c3a606bda5d229bbacc0369d2e6b8b4981e
CodeSonia <sonia_ah@icloud.com>
2021-10-20 16:37:56 +0000

    Feat: created animated hamburger with lib and JS

---
+++ assets/js/hamburger.js
@@ -0,0 +1,5 @@
+// Use jQuery to target .hamburger
+$(".hamburger").click(function() {
+  // use jQuery toggleClass to toggle between add and removing one class
+  $(".hamburger").toggleClass("is-active");
+})
