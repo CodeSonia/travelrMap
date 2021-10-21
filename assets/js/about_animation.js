b6a12ef9bd3bcc5e180da818753d05ec137861c1
CodeSonia <sonia_ah@icloud.com>
2021-10-21 19:18:16 +0000

    general save

---
+++ assets/js/about_animation.js
@@ -0,0 +1,12 @@
+let aboutAnimation = bodymovin.loadAnimation({
+  container: document.getElementById("about-animation"),
+  path: "assets/json/about/lf30_editor_a1hi94z8.json",
+  renderer: "svg",
+  loop: false,
+  autoplay: true,
+  name: "About Animation"
+})
+
+
+
+    
