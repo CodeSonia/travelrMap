aa7a4e5b90a74aa1d4ad0cff5601603f5e29b391
CodeSonia <sonia_ah@icloud.com>
2021-10-26 14:43:03 +0000

    Fix: on mobile, when clicking on navbar item, it'll now close

--- assets/js/hamburger.js
+++ assets/js/hamburger.js
@@ -4,4 +4,10 @@ $(document).ready(function() {
     // use jQuery toggleClass to toggle between add and removing one class
     $(".hamburger").toggleClass("is-active");
   });
+
+  /* target navbar items and when a user clicks on it, it'll then close the hamburger icon*/
+  $(".navbar-nav li a").on("click", function(){
+    $(".hamburger").click();
+  })
 });
+
